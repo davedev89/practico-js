b58fa62de443a3f617647643531a17cbb63f5ce5
davedev89 <davidcarrevedo@gmail.com>
2021-09-10 15:15:13 -0300

    Se agrega moda, salarios, e index juntando proyectos

---
+++ salarios.js
@@ -0,0 +1,101 @@
+const colombia = []
+
+colombia.push({
+     name: "Juan",
+     salary: 100
+});
+
+colombia.push({
+     name: "Luis",
+     salary: 1100
+});
+
+colombia.push({
+     name: "Pedro",
+     salary: 10000
+});
+
+colombia.push({
+     name: "Maria",
+     salary: 500
+});
+
+colombia.push({
+     name: "Carla",
+     salary: 800
+});
+
+colombia.push({
+     name: "David",
+     salary: 700
+});
+
+colombia.push({
+     name: "Damaris",
+     salary: 3000
+});
+
+colombia.push({
+     name: "Titi",
+     salary: 150
+});
+
+colombia.push({
+     name: "Nacho",
+     salary: 250
+});
+
+colombia.push({
+     name: "Jazmin",
+     salary: 1170
+});
+
+colombia.push({
+     name: "Sere",
+     salary: 100
+});
+
+colombia.push({
+     name: "Carol",
+     salary: 2200
+});
+
+colombia.push({
+     name: "Maxi",
+     salary: 10011
+});
+
+colombia.push({
+     name: "Vange",
+     salary: 450
+});
+
+colombia.push({
+     name: "Ariel",
+     salary: 970
+});
+
+colombia.push({
+     name: "Cuca",
+     salary: 1300
+});
+
+colombia.push({
+     name: "Bart",
+     salary: 3000
+});
+
+colombia.push({
+     name: "Oli",
+     salary: 80
+});
+
+colombia.push({
+     name: "Jorge",
+     salary: 6500
+});
+
+colombia.push({
+     name: "Sandra",
+     salary: 3700
+});
